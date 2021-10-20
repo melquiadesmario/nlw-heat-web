4e119aaa791f13f3db865dacbbf940e2cb868d39
melquiadesmario <melquiadesm@gmail.com>
2021-10-20 13:48:22 -0300

    MessageList Styling Start

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,52 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt='DoWhile 2021' />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamoo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/melquiadesmario.png' alt='avatar' />
+            </div>
+            <span>Melquíades Mário</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamoo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/melquiadesmario.png' alt='avatar' />
+            </div>
+            <span>Melquíades Mário</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamoo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/melquiadesmario.png' alt='avatar' />
+            </div>
+            <span>Melquíades Mário</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
