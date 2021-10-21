d6ce320aa3b740e4a74b459703ab99a95710fa9b
melquiadesmario <melquiadesm@gmail.com>
2021-10-20 22:49:58 -0300

    SignOut

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -21,6 +21,7 @@ type User = {
 type AuthContextData = {
   user: User | null;
   signInUrl: string;
+  signOut: () => void;
 };
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -44,6 +45,11 @@ export function AuthProvider(props: AuthProvider) {
     localStorage.setItem('@dowhile:token', token);
 
     setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem('@dowhile:token');
   }
 
   useEffect(() => {
@@ -72,7 +78,7 @@ export function AuthProvider(props: AuthProvider) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signInUrl, user }}>
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
       {props.children}
     </AuthContext.Provider>
   );
